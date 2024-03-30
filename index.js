47fc6a9d1508f5b82bbbe25c38987eb5191a2f60
VVchari <125472557+VVchari@users.noreply.github.com>
2024-03-30 12:14:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const express = require('express')
 const app = express()
 const mongoDB=require('./db')
-
+const cors = require('cors')
 mongoDB();
 
 app.get('/', (req, res)=> {
   res.send('Hello world')
 })
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","https://mernback-em0b.onrender.com/")
-  res.header("Access-Control-Allow-Headers",
-  "Orgin,X-Requested-With,Content-Type,Accept")
-  next()
-})
+app.use(cors())
 app.use(express.json())
 app.use('/api',require("./Routes/User"))
 app.use('/api',require("./Routes/DisplayData"))
